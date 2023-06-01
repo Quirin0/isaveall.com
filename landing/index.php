60f609d94f4ee5ea7aa4f3285e878c59501f0c58
Quirin0 <matheusviniciusquirino@gmail.com>
2023-06-01 01:38:50 -0300

    teste

--- landing/index.php
+++ landing/index.php
@@ -1,18 +1,23 @@
 <?php
-$url = "https://www.instagram.com/reel/CsJcnPjt53g/?__a=1&__d=dis";
 
-$ch = curl_init();
+$curl = curl_init();
 
-curl_setopt($ch, CURLOPT_URL, $url);
-curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+curl_setopt_array($curl, array(
+  CURLOPT_URL => 'https://www.instagram.com/reel/CsJcnPjt53g/?__a=1&__d=dis',
+  CURLOPT_RETURNTRANSFER => true,
+  CURLOPT_ENCODING => '',
+  CURLOPT_MAXREDIRS => 10,
+  CURLOPT_TIMEOUT => 0,
+  CURLOPT_FOLLOWLOCATION => true,
+  CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+  CURLOPT_CUSTOMREQUEST => 'GET',
+  CURLOPT_HTTPHEADER => array(
+    'Cookie: csrftoken=OsosgC3yUIFKwLyra4uEVPK2kZw93CgE; ig_did=6CD51340-2BFE-4DE9-990F-CC59F11EAC50; ig_nrcb=1; mid=ZHgWFQAEAAFSKu1JqwYoUHEuX4Uk'
+  ),
+));
 
-$response = curl_exec($ch);
+$response = curl_exec($curl);
 
-if ($response === false) {
-    echo "Falha ao fazer a requisição.";
-} else {
-    echo $response;
-}
-
-curl_close($ch);
+curl_close($curl);
+echo $response;
 ?>
