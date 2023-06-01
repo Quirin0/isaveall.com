d9fd01bc07377b3e403b5360fd8a1b93737c3739
Quirin0 <matheusviniciusquirino@gmail.com>
2023-06-01 01:45:28 -0300

    teste

--- landing/index.php
+++ landing/index.php
@@ -1,19 +1,23 @@
 <?php
-$url = "https://www.instagram.com/reel/CsJcnPjt53g/?__a=1&__d=dis";
 
-$ch = curl_init();
+$curl = curl_init();
 
-curl_setopt($ch, CURLOPT_URL, $url);
-curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-curl_setopt($ch, CURLOPT_USERAGENT, 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/95.0.4638.69 Safari/537.36');
+curl_setopt_array($curl, array(
+  CURLOPT_URL => 'https://www.instagram.com/reel/CsJcnPjt53g/?__a=1&__d=dis',
+  CURLOPT_RETURNTRANSFER => true,
+  CURLOPT_ENCODING => '',
+  CURLOPT_MAXREDIRS => 10,
+  CURLOPT_TIMEOUT => 0,
+  CURLOPT_FOLLOWLOCATION => true,
+  CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+  CURLOPT_CUSTOMREQUEST => 'GET',
+  CURLOPT_USERAGENT => 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/95.0.4638.69 Safari/537.36'),
+  CURLOPT_HTTPHEADER => array(
+    'Cookie: csrftoken=OsosgC3yUIFKwLyra4uEVPK2kZw93CgE; ig_did=6CD51340-2BFE-4DE9-990F-CC59F11EAC50; ig_nrcb=1; mid=ZHgWFQAEAAFSKu1JqwYoUHEuX4Uk'
+  ),
+));
 
-$response = curl_exec($ch);
+$response = curl_exec($curl);
 
-if ($response === false) {
-    echo "Falha ao fazer a requisição.";
-} else {
-    echo $response;
-}
-
-curl_close($ch);
-?>
+curl_close($curl);
+echo $response;
