09fb4e13866140b72c335f7ea873d7051c344bf9
Quirin0 <matheusviniciusquirino@gmail.com>
2023-06-13 11:45:46 -0300

    .

--- dl/downloadFile.php
+++ dl/downloadFile.php
@@ -1,6 +1,16 @@
 <?php
 // URL do vídeo a ser baixado
 $url = $_POST['url'];
+//Define header information
+header('Content-Description: File Transfer');
+header('Content-Type: application/octet-stream');
+header("Cache-Control: no-cache, must-revalidate");
+header("Expires: 0");
+header('Content-Disposition: attachment; filename="'.basename($url).'"');
+header('Content-Length: ' . filesize($url));
+header('Pragma: public');
 
+//Clear system output buffer
+flush();
 // Faz o download do vídeo e envia como resposta
 readfile($url);
